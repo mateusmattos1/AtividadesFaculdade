93a297e621d95b0b68e4a5ea3d833dfd6f5622c8
Mateus de Mattos <100395146+mateusmattos1@users.noreply.github.com>
2022-07-08 15:45:08 -0300

    Cursos complementares

---
+++ qualquer/CursosComplementares/Balta/CSharp/BaltaFundamentoObjeto/BaltaFundamentoObjeto/ContentContext/Content.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaltaFundamentoObjeto.ContentContext
+{
+    internal abstract class Content
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+
+        public Content()
+        {
+            Id = Guid.NewGuid(); 
+        }
+    }
+}
---
+++ qualquer/CursosComplementares/Balta/CSharp/BaltaFundamentoObjeto/BaltaFundamentoObjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using BaltaFundamentoObjeto.ContentContext;
+using System;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+
+        }
+    }
+}
